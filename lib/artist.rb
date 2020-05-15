76cf4ec79f63e1db183e4f9565b14ddb3a257815
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-05-15 02:24:25 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,27 @@
-class Artist
+require 'pry'
+class Artist 
   attr_accessor :name
+  
   @@all = []
+  
   def initialize(name)
     @name = name
     @@all << self
   end
+  
   def new_song(name, genre)
-    Song
+    Song.new(name, self, genre)
+  end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def genres
+    genres =[]
+    self.songs.each {|song| genres << song.genre}
+    genres
+  end
   def self.all
     @@all
   end
