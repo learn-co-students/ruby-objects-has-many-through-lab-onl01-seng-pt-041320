2ea3955d9b5f292e54a23e781f2930fde7077b60
celestesmthomas <celestesmthomas@gmail.com>
2020-05-19 21:33:44 -0700

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,28 @@
+require 'pry'
+
+class Genre
+    attr_accessor :name, :song, :artist
+
+@@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+
+    def songs
+        Song.all.select { |song| song.genre == self }
+    end
+
+    def self.all
+        @@all
+    end
+
+    def artists
+        #binding.pry
+        self.songs.map { |song| song.artist }
+    end
+
+
+end
